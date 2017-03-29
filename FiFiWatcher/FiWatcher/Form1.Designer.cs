dd53aa2eea03a1f65f00c0ad58175bb0ea69585c
Tomoyuki Nohara a.k.a FiFi <peace098beat.t@gmail.com>
2017-03-30 07:56:08 +0900

    Add files via upload

---
+++ FiFiWatcher/FiWatcher/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace FiWatcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label_okFile = new System.Windows.Forms.Label();
+            this.btn_okFile = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn_watchStart = new System.Windows.Forms.Button();
+            this.btn_watchStop = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label_watcher = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label_okFile
+            // 
+            this.label_okFile.AutoSize = true;
+            this.label_okFile.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label_okFile.Location = new System.Drawing.Point(88, 57);
+            this.label_okFile.Name = "label_okFile";
+            this.label_okFile.Size = new System.Drawing.Size(117, 12);
+            this.label_okFile.TabIndex = 0;
+            this.label_okFile.Text = "Please Select OK File";
+            // 
+            // btn_okFile
+            // 
+            this.btn_okFile.Location = new System.Drawing.Point(27, 52);
+            this.btn_okFile.Name = "btn_okFile";
+            this.btn_okFile.Size = new System.Drawing.Size(55, 23);
+            this.btn_okFile.TabIndex = 1;
+            this.btn_okFile.Text = "browse";
+            this.btn_okFile.UseVisualStyleBackColor = true;
+            this.btn_okFile.Click += new System.EventHandler(this.btn_okFile_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(138, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "OKファイルを選択してください";
+            // 
+            // btn_watchStart
+            // 
+            this.btn_watchStart.Location = new System.Drawing.Point(45, 189);
+            this.btn_watchStart.Name = "btn_watchStart";
+            this.btn_watchStart.Size = new System.Drawing.Size(134, 58);
+            this.btn_watchStart.TabIndex = 3;
+            this.btn_watchStart.Text = "監視開始";
+            this.btn_watchStart.UseVisualStyleBackColor = true;
+            this.btn_watchStart.Click += new System.EventHandler(this.btn_watchStart_Click);
+            // 
+            // btn_watchStop
+            // 
+            this.btn_watchStop.Location = new System.Drawing.Point(211, 189);
+            this.btn_watchStop.Name = "btn_watchStop";
+            this.btn_watchStop.Size = new System.Drawing.Size(135, 58);
+            this.btn_watchStop.TabIndex = 4;
+            this.btn_watchStop.Text = "監視停止";
+            this.btn_watchStop.UseVisualStyleBackColor = true;
+            this.btn_watchStop.Click += new System.EventHandler(this.btn_watchStop_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.textBox1.Location = new System.Drawing.Point(27, 267);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(373, 111);
+            this.textBox1.TabIndex = 5;
+            // 
+            // label_watcher
+            // 
+            this.label_watcher.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label_watcher.Font = new System.Drawing.Font("MS UI Gothic", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label_watcher.Location = new System.Drawing.Point(27, 101);
+            this.label_watcher.Name = "label_watcher";
+            this.label_watcher.Size = new System.Drawing.Size(387, 54);
+            this.label_watcher.TabIndex = 6;
+            this.label_watcher.Text = "監視停止中";
+            this.label_watcher.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(426, 394);
+            this.Controls.Add(this.label_watcher);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btn_watchStop);
+            this.Controls.Add(this.btn_watchStart);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btn_okFile);
+            this.Controls.Add(this.label_okFile);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "FiFiFile Watcher";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label_okFile;
+        private System.Windows.Forms.Button btn_okFile;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn_watchStart;
+        private System.Windows.Forms.Button btn_watchStop;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label_watcher;
+    }
+}
+
---
+++ FiFiWatcher/FiWatcher/Form1.cs
@@ -0,0 +1,155 @@
+﻿//
+// フォルダ監視
+// http://dobon.net/vb/dotnet/file/filesystemwatcher.html
+//
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FiWatcher
+{
+    public partial class Form1 : Form
+    {
+        // Watcher generate
+        private System.IO.FileSystemWatcher watcher = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // 監視ラベル
+            Change_State_isWatch(false);
+
+            // ボタン初期状態(最初だけ)
+            btn_watchStart.Enabled = false;
+
+            // 実行フォルダを初期地
+            openFileDialog1.InitialDirectory = System.AppDomain.CurrentDomain.BaseDirectory.TrimEnd('\\');
+
+        }
+
+        private void Change_State_isWatch(bool watching)
+        {
+            if (watching)
+            {
+                // 監視中
+                btn_okFile.Enabled = false;
+                btn_watchStart.Enabled = false;
+                btn_watchStop.Enabled = true;
+                label_watcher.Text = "監視中";
+                label_watcher.ForeColor = Color.Red;
+            }
+            else
+            {
+                // 非監視
+                btn_okFile.Enabled = true;
+                btn_watchStart.Enabled = true;
+                btn_watchStop.Enabled = false;
+                label_watcher.Text = "監視停止中";
+                label_watcher.ForeColor = Color.Gray;
+            }
+        }
+
+        // 監視ファイルの選択ボタン
+        private void btn_okFile_Click(object sender, EventArgs e)
+        {
+            // ダイアログオープン
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                label_okFile.Text = openFileDialog1.FileName;
+                btn_watchStart.Enabled = true;
+            }
+        }
+
+        // 監視開始ボタン
+        private void btn_watchStart_Click(object sender, EventArgs e)
+        {
+            // ボタンの状態変更
+            Change_State_isWatch(true);
+
+            if (watcher != null) return; // 監視中は動作しない
+
+            watcher = new System.IO.FileSystemWatcher();
+
+            // 監視ディレクトリ
+            watcher.Path = System.IO.Path.GetDirectoryName(openFileDialog1.FileName);
+
+            watcher.NotifyFilter = 
+                (System.IO.NotifyFilters.LastAccess
+                | System.IO.NotifyFilters.LastWrite
+                | System.IO.NotifyFilters.FileName
+                | System.IO.NotifyFilters.DirectoryName);
+
+            // 監視ファイル
+            String watch_file = System.IO.Path.GetFileName(openFileDialog1.FileName);
+            watcher.Filter = watch_file; //全てのファイルを監視
+
+            // UIのスレッドにマーシャリングをする
+            watcher.SynchronizingObject = this;
+
+            //イベントハンドラの追加
+            watcher.Changed += new System.IO.FileSystemEventHandler(watcher_Changed);
+            watcher.Created += new System.IO.FileSystemEventHandler(watcher_Changed);
+            watcher.Deleted += new System.IO.FileSystemEventHandler(watcher_Changed);
+
+            //監視を開始する
+            watcher.EnableRaisingEvents = true;
+            debug_log(watch_file + @"の監視を開始しました");
+
+        }
+
+        // 監視終了ボタン
+        private void btn_watchStop_Click(object sender, EventArgs e)
+        {
+            // ボタンの状態変更
+            Change_State_isWatch(false);
+
+            // 監視終了
+            watcher.EnableRaisingEvents = false;
+            watcher.Dispose();
+            watcher = null;
+
+            // デバッグ
+            debug_log("監視を終了しました");
+        }
+
+        //イベントハンドラ
+        private void watcher_Changed(System.Object source,
+            System.IO.FileSystemEventArgs e)
+        {
+
+            String watch_file = System.IO.Path.GetFileName(openFileDialog1.FileName);
+
+            switch (e.ChangeType)
+            {
+                case System.IO.WatcherChangeTypes.Changed:
+                    debug_log("ファイル 「" + watch_file + "」が変更されました。");
+                    MessageBox.Show("ファイル 「" + watch_file + "」が変更されました。");
+                    break;
+                case System.IO.WatcherChangeTypes.Created:
+                    debug_log("ファイル 「" + watch_file + "」が作成されました。");
+                    MessageBox.Show("ファイル 「" + watch_file + "」が作成されました。");
+                    break;
+                case System.IO.WatcherChangeTypes.Deleted:
+                    debug_log("ファイル 「" + watch_file + "」が削除されました。");
+                    MessageBox.Show("ファイル 「" + watch_file + "」が削除されました。");
+                    break;
+            }
+        }
+
+        // debug_log
+        private void debug_log(String msg)
+        {
+            textBox1.AppendText(msg+"\n");
+
+        }
+
+    }
+}
