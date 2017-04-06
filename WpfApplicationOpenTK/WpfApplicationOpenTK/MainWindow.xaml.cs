b1b80e8463ed4356dd50ca4dd73c8f2465b8a417
Tomoyuki Nohara a.k.a FiFi <peace098beat.t@gmail.com>
2017-04-06 16:38:36 +0900

    Add files via upload

---
+++ WpfApplicationOpenTK/WpfApplicationOpenTK/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using System.Windows;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+
+
+namespace WpfApplicationOpenTK
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+
+
+    }
+}
