c91ef2c07080b679b82f3174d2f2418a8b5dce4f
Tomoyuki Nohara a.k.a FiFi <peace098beat.t@gmail.com>
2017-04-26 15:08:37 +0900

    Add files via upload

---
+++ BaseForm/BaseForm/Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace BaseForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BottomToolStripPanel = new System.Windows.Forms.ToolStripPanel();
+            this.TopToolStripPanel = new System.Windows.Forms.ToolStripPanel();
+            this.RightToolStripPanel = new System.Windows.Forms.ToolStripPanel();
+            this.LeftToolStripPanel = new System.Windows.Forms.ToolStripPanel();
+            this.ContentPanel = new System.Windows.Forms.ToolStripContentPanel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.FileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ExitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.SettingToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.HelpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ToolStripMenuItemReadme = new System.Windows.Forms.ToolStripMenuItem();
+            this.ToolStripMenuItemWEB = new System.Windows.Forms.ToolStripMenuItem();
+            this.ToolStripMenuItemVersion = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripContainer1 = new System.Windows.Forms.ToolStripContainer();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.menuStrip1.SuspendLayout();
+            this.toolStripContainer1.BottomToolStripPanel.SuspendLayout();
+            this.toolStripContainer1.TopToolStripPanel.SuspendLayout();
+            this.toolStripContainer1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // BottomToolStripPanel
+            // 
+            this.BottomToolStripPanel.Location = new System.Drawing.Point(0, 0);
+            this.BottomToolStripPanel.Name = "BottomToolStripPanel";
+            this.BottomToolStripPanel.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            this.BottomToolStripPanel.RowMargin = new System.Windows.Forms.Padding(3, 0, 0, 0);
+            this.BottomToolStripPanel.Size = new System.Drawing.Size(0, 0);
+            // 
+            // TopToolStripPanel
+            // 
+            this.TopToolStripPanel.Location = new System.Drawing.Point(0, 0);
+            this.TopToolStripPanel.Name = "TopToolStripPanel";
+            this.TopToolStripPanel.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            this.TopToolStripPanel.RowMargin = new System.Windows.Forms.Padding(3, 0, 0, 0);
+            this.TopToolStripPanel.Size = new System.Drawing.Size(0, 0);
+            // 
+            // RightToolStripPanel
+            // 
+            this.RightToolStripPanel.Location = new System.Drawing.Point(0, 0);
+            this.RightToolStripPanel.Name = "RightToolStripPanel";
+            this.RightToolStripPanel.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            this.RightToolStripPanel.RowMargin = new System.Windows.Forms.Padding(3, 0, 0, 0);
+            this.RightToolStripPanel.Size = new System.Drawing.Size(0, 0);
+            // 
+            // LeftToolStripPanel
+            // 
+            this.LeftToolStripPanel.Location = new System.Drawing.Point(0, 0);
+            this.LeftToolStripPanel.Name = "LeftToolStripPanel";
+            this.LeftToolStripPanel.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            this.LeftToolStripPanel.RowMargin = new System.Windows.Forms.Padding(3, 0, 0, 0);
+            this.LeftToolStripPanel.Size = new System.Drawing.Size(0, 0);
+            // 
+            // ContentPanel
+            // 
+            this.ContentPanel.Size = new System.Drawing.Size(489, 321);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Dock = System.Windows.Forms.DockStyle.None;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.FileToolStripMenuItem,
+            this.SettingToolStripMenuItem,
+            this.HelpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(489, 26);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // FileToolStripMenuItem
+            // 
+            this.FileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ExitToolStripMenuItem});
+            this.FileToolStripMenuItem.Name = "FileToolStripMenuItem";
+            this.FileToolStripMenuItem.Size = new System.Drawing.Size(85, 22);
+            this.FileToolStripMenuItem.Text = "ファイル(&F)";
+            // 
+            // ExitToolStripMenuItem
+            // 
+            this.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem";
+            this.ExitToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.F4)));
+            this.ExitToolStripMenuItem.Size = new System.Drawing.Size(166, 22);
+            this.ExitToolStripMenuItem.Text = "終了(&X)";
+            this.ExitToolStripMenuItem.Click += new System.EventHandler(this.ExitToolStripMenuItem_Click);
+            // 
+            // SettingToolStripMenuItem
+            // 
+            this.SettingToolStripMenuItem.Name = "SettingToolStripMenuItem";
+            this.SettingToolStripMenuItem.Size = new System.Drawing.Size(62, 22);
+            this.SettingToolStripMenuItem.Text = "設定(&S)";
+            // 
+            // HelpToolStripMenuItem
+            // 
+            this.HelpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ToolStripMenuItemReadme,
+            this.ToolStripMenuItemWEB,
+            this.ToolStripMenuItemVersion});
+            this.HelpToolStripMenuItem.Name = "HelpToolStripMenuItem";
+            this.HelpToolStripMenuItem.Size = new System.Drawing.Size(75, 22);
+            this.HelpToolStripMenuItem.Text = "ヘルプ(&H)";
+            // 
+            // ToolStripMenuItemReadme
+            // 
+            this.ToolStripMenuItemReadme.Name = "ToolStripMenuItemReadme";
+            this.ToolStripMenuItemReadme.Size = new System.Drawing.Size(195, 22);
+            this.ToolStripMenuItemReadme.Text = "READMEを表示(&R)";
+            this.ToolStripMenuItemReadme.Click += new System.EventHandler(this.ToolStripMenuItemReadme_Click);
+            // 
+            // ToolStripMenuItemWEB
+            // 
+            this.ToolStripMenuItemWEB.Name = "ToolStripMenuItemWEB";
+            this.ToolStripMenuItemWEB.Size = new System.Drawing.Size(195, 22);
+            this.ToolStripMenuItemWEB.Text = "Webサイトを表示(&W)";
+            this.ToolStripMenuItemWEB.Click += new System.EventHandler(this.ToolStripMenuItemWEB_Click);
+            // 
+            // ToolStripMenuItemVersion
+            // 
+            this.ToolStripMenuItemVersion.Name = "ToolStripMenuItemVersion";
+            this.ToolStripMenuItemVersion.Size = new System.Drawing.Size(195, 22);
+            this.ToolStripMenuItemVersion.Text = "バージョン情報(&V)";
+            this.ToolStripMenuItemVersion.Click += new System.EventHandler(this.ToolStripMenuItemVersion_Click);
+            // 
+            // toolStripContainer1
+            // 
+            // 
+            // toolStripContainer1.BottomToolStripPanel
+            // 
+            this.toolStripContainer1.BottomToolStripPanel.Controls.Add(this.statusStrip1);
+            // 
+            // toolStripContainer1.ContentPanel
+            // 
+            this.toolStripContainer1.ContentPanel.Size = new System.Drawing.Size(489, 297);
+            this.toolStripContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.toolStripContainer1.LeftToolStripPanelVisible = false;
+            this.toolStripContainer1.Location = new System.Drawing.Point(0, 0);
+            this.toolStripContainer1.Name = "toolStripContainer1";
+            this.toolStripContainer1.RightToolStripPanelVisible = false;
+            this.toolStripContainer1.Size = new System.Drawing.Size(489, 346);
+            this.toolStripContainer1.TabIndex = 0;
+            this.toolStripContainer1.Text = "toolStripContainer1";
+            // 
+            // toolStripContainer1.TopToolStripPanel
+            // 
+            this.toolStripContainer1.TopToolStripPanel.Controls.Add(this.menuStrip1);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Dock = System.Windows.Forms.DockStyle.None;
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 0);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(489, 23);
+            this.statusStrip1.TabIndex = 0;
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(443, 18);
+            this.toolStripStatusLabel1.Spring = true;
+            this.toolStripStatusLabel1.Text = "rady";
+            this.toolStripStatusLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(489, 346);
+            this.Controls.Add(this.toolStripContainer1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.toolStripContainer1.BottomToolStripPanel.ResumeLayout(false);
+            this.toolStripContainer1.BottomToolStripPanel.PerformLayout();
+            this.toolStripContainer1.TopToolStripPanel.ResumeLayout(false);
+            this.toolStripContainer1.TopToolStripPanel.PerformLayout();
+            this.toolStripContainer1.ResumeLayout(false);
+            this.toolStripContainer1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStripPanel BottomToolStripPanel;
+        private System.Windows.Forms.ToolStripPanel TopToolStripPanel;
+        private System.Windows.Forms.ToolStripPanel RightToolStripPanel;
+        private System.Windows.Forms.ToolStripPanel LeftToolStripPanel;
+        private System.Windows.Forms.ToolStripContentPanel ContentPanel;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem FileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ExitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem SettingToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem HelpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ToolStripMenuItemReadme;
+        private System.Windows.Forms.ToolStripMenuItem ToolStripMenuItemWEB;
+        private System.Windows.Forms.ToolStripMenuItem ToolStripMenuItemVersion;
+        private System.Windows.Forms.ToolStripContainer toolStripContainer1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+    }
+}
+
---
+++ BaseForm/BaseForm/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BaseForm
+{
+    public partial class Form1 : Form
+    {
+
+        // Member Variable
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// フォームのロード時に呼び出される関数
+        /// </summary>
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // ?
+            this.KeyPreview = true;
+            // タイトルにアプリ名を表示
+            this.Text = Application.ProductName +" "+Application.ProductVersion;
+            // ウィンドウサイズと位置の保存
+            this.MinimumSize = new System.Drawing.Size(300, 100);
+            this.Size = Properties.Settings.Default.Size;
+            this.Location = Properties.Settings.Default.Location;
+
+            // ウィンドウが画面の外にある場合の処理
+            if (this.Left < Screen.GetWorkingArea(this).Left) this.Left = 100;
+            if (this.Left >= Screen.GetWorkingArea(this).Right) this.Left = 100;
+
+            if (this.Top < Screen.GetWorkingArea(this).Top) this.Top = 100;
+            if (this.Top >= Screen.GetWorkingArea(this).Bottom) this.Top = 100;
+        }
+
+        /// <summary>
+        /// メニュー -> ファイル -> 終了(X)
+        /// </summary>
+        private void ExitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //this.Close();
+            Application.Exit();
+        }
+
+        /// <summary>
+        /// フォームが閉じられたときに呼び出される関数
+        /// </summary>
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            // 画面の位置とサイズを保存
+            this.WindowState = FormWindowState.Normal;
+            Properties.Settings.Default.Size = this.Size;
+            Properties.Settings.Default.Location = this.Location;
+            // 設定保存
+            Properties.Settings.Default.Save();
+        }
+
+        /// <summary>
+        /// ヴァージョン情報をクリック
+        /// </summary>
+        private void ToolStripMenuItemVersion_Click(object sender, EventArgs e)
+        {
+            string s = "";
+
+            s = Application.ProductName + " " + Application.ProductVersion + Environment.NewLine;
+
+            // コピーライトを表示
+            var fileVrsionInfo = (System.Diagnostics.FileVersionInfo.GetVersionInfo(Application.ExecutablePath));
+            string copyright = fileVrsionInfo.LegalCopyright.ToString();
+            s += copyright + Environment.NewLine + Environment.NewLine;
+
+            // 実行ファイルを表示
+            s += "実行ファイル : " + Environment.NewLine +
+                Application.ExecutablePath + Environment.NewLine;
+
+            // 実行プロセスを表示
+            string bit;
+            if (Environment.Is64BitProcess) bit = "64";
+            else bit = "32";
+            s += "(" + bit + "ビット・プロセスとして稼動)" + Environment.NewLine + Environment.NewLine;
+
+            // OSの情報を表示
+            var myComputer = new Microsoft.VisualBasic.Devices.Computer();
+            s += "オペレーティングシステム:" + Environment.NewLine;
+            s += myComputer.Info.OSFullName + " " + myComputer.Info.OSVersion + " ";
+            string osbit;
+            if (Environment.Is64BitOperatingSystem) osbit = "64";
+            else osbit = "32";
+            s += osbit + "ビット";
+
+            // ヴァージョン情報を表示
+            MessageBox.Show(s, "バージョン情報");
+        }
+
+        /// <summary>
+        ///  READMEを表示
+        /// </summary>
+        private void ToolStripMenuItemReadme_Click(object sender, EventArgs e)
+        {
+            // ファイルから読み込む
+            string s = System.IO.Path.GetDirectoryName(Application.ExecutablePath);
+            s = System.IO.Path.Combine(s, "README.txt");
+            if (System.IO.File.Exists(s))
+                System.Diagnostics.Process.Start(s);
+            else
+                MessageBox.Show(s + "が見つかりません", "エラー");
+        }
+        /// <summary>
+        /// README.htmlを表示
+        /// </summary>
+        private void ToolStripMenuItemWEB_Click(object sender, EventArgs e)
+        {
+            // ファイルから読み込む
+            string s = System.IO.Path.GetDirectoryName(Application.ExecutablePath);
+            s = System.IO.Path.Combine(s, "README.html");
+            if (System.IO.File.Exists(s))
+                System.Diagnostics.Process.Start(s);
+            else
+                MessageBox.Show(s + "が見つかりません", "エラー");
+        }
+    }
+}
